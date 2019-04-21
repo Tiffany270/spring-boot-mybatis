0103547be2a3075635a8be59e08a9cec1296196f
yiki <1033822272@qq.com>
2019-04-21 21:47:22 +0800

    'Post参数多对象JSON嵌套转换'

--- src/main/java/yiki/mybatis/bean/Data.java
+++ src/main/java/yiki/mybatis/bean/Data.java
@@ -2,6 +2,31 @@ package yiki.mybatis.bean;
 
 public class Data {
 
-    int errorcode;
+    private int t;
+    private String b;
 
+    @Override
+    public String toString() {
+        return "Data{" +
+         "t=" + t +
+         ", b='" + b + '\'' +
+         '}';
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public int getT() {
+
+        return t;
+    }
+
+    public void setT(int t) {
+        this.t = t;
+    }
 }
--- src/main/java/yiki/mybatis/controller/HelloController.java
+++ src/main/java/yiki/mybatis/controller/HelloController.java
@@ -1,12 +1,20 @@
 package yiki.mybatis.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import yiki.mybatis.bean.Data;
 import yiki.mybatis.bean.Firm;
 import yiki.mybatis.bean.User;
 import yiki.mybatis.mapper.FirmMapper;
 import yiki.mybatis.mapper.UserMapper;
 import yiki.mybatis.mapper.UserYmlMapper;
+import yiki.mybatis.util.JsonUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 
 /*
@@ -16,31 +24,54 @@ import yiki.mybatis.mapper.UserYmlMapper;
 @ResponseBody
 @RestController
 public class HelloController {
-    @RequestMapping("/hello")
-    public String hello() {
-        return "hello world";
-    }
+ @RequestMapping("/hello")
+ public String hello() {
+  return "hello world";
+ }
 
-    @Autowired
-    UserMapper userMapper;
-    @Autowired
-    UserYmlMapper userYmlMapper;
-    @Autowired
-    FirmMapper firmMapper;
+ @Autowired
+ UserMapper userMapper;
+ @Autowired
+ UserYmlMapper userYmlMapper;
+ @Autowired
+ FirmMapper firmMapper;
 
 
-    @GetMapping("/user/{id}")
-    public User getUser(@PathVariable("id") Integer id) {
-        return userMapper.getUserById(id);
-    }
+ @GetMapping("/user/{id}")
+ public User getUser(@PathVariable("id") Integer id) {
+  return userMapper.getUserById(id);
+ }
 
-    @GetMapping("/userxml/{uid}")
-    public User getxmlUser(@PathVariable("uid") Integer id) {
-        return userYmlMapper.getUserById(id);
-    }
+ @GetMapping("/userxml/{uid}")
+ public User getxmlUser(@PathVariable("uid") Integer id) {
+  return userYmlMapper.getUserById(id);
+ }
 
-    @GetMapping("/firmtest/{cid}")
-    public Firm getfirm(@PathVariable("cid") Integer cid) {
-        return firmMapper.getFirmById(cid);
-    }
+ @GetMapping("/firmtest/{cid}")
+ public Firm getfirm(@PathVariable("cid") Integer cid) {
+  return firmMapper.getFirmById(cid);
+ }
+
+ @PostMapping("/test")
+ public int test(@RequestBody Map<String, Object> models) {
+  Map<String, Object> userobj = (Map<String, Object>) models.get("user");
+  ArrayList<Map<String, Object>> test = (ArrayList<Map<String, Object>>) models.get("test");
+
+
+  try {
+   User user = JsonUtil.map2obj(userobj, User.class);
+
+   for (Map<String, Object> item : test) {
+    Data data = JsonUtil.map2obj(item,Data.class);
+    System.out.println(data);
+    System.out.println(data.getB());
+   }
+
+   System.out.println(user.getUname());
+  } catch (Exception e) {
+   System.out.println(e);
+  }
+  return 1;
+ }
+
 }
