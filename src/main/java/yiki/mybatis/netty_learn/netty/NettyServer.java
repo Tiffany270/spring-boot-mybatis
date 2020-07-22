f59fd93ecc9fb18d3988baea5a3debab3e9aea28
yiki <yiki270@foxmail.com>
2020-07-22 16:18:17 +0800

    'netty快点学完吧!'

--- src/main/java/yiki/mybatis/netty_learn/netty/NettyServer.java
+++ src/main/java/yiki/mybatis/netty_learn/netty/NettyServer.java
@@ -18,39 +18,46 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 public class NettyServer {
     public void start() throws Exception {
 
+
+
         /* create BossGroup and WorkGroup,
          these are thread group, both is loop
          'boss' is to handle request of connection
          'woke' is to handle real work with client */
         NioEventLoopGroup bossGroup = new NioEventLoopGroup();
         NioEventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
 
-        // create the start obj of server, and configure params
-        ServerBootstrap bootstrap = new ServerBootstrap();
+            // create the start obj of server, and configure params
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            // linked module configuration
+            bootstrap.group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    //connection nums of thread waiting
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    //keep alive status
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    //set handle to the pip of specif woke eventLoop
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        // set handler for pipeline
+                        @Override
+                        protected void initChannel(SocketChannel sch) throws Exception {
+                            sch.pipeline().addLast(new NettyServerHandler());
+                        }
+                    });
 
-        // linked module configuration
-        bootstrap.group(bossGroup, workGroup)
-                .channel(NioServerSocketChannel.class)
-                //connection nums of thread waiting
-                .option(ChannelOption.SO_BACKLOG, 128)
-                //keep alive status
-                .childOption(ChannelOption.SO_KEEPALIVE, true)
-                //set handle to the pip of specif woke eventLoop
-                .childHandler(new ChannelInitializer<SocketChannel>() {
-                    // set handler for pipeline
-                    @Override
-                    protected void initChannel(SocketChannel sch) throws Exception {
-                        sch.pipeline().addLast(new NettyServerHandler());
-                    }
-                });
+            System.out.println("server is ready ....");
 
-        System.out.println("server is ready ....");
+            // bind port and set sync
+            ChannelFuture cf = bootstrap.bind(6668).sync();
 
-        // bind port and set sync
-        ChannelFuture cf = bootstrap.bind(6668).sync();
+            //listen the close event for pip
+            //(when you close your channel the serve would know it)
+            cf.channel().closeFuture().sync();
+        } finally {
+            workGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
 
-        //listen the close event for pip
-        //(when you close your channel the serve would know it)
-        cf.channel().closeFuture().sync();
+        }
     }
 }
